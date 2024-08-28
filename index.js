efde424c1f6ddb6a756b6fd06f64968d18ce3d7d
aldogerard <aldogerard12@gmail.com>
2024-08-28 21:00:17 +0700

    FIX : fixing cors in json 5

--- index.js
+++ index.js
@@ -18,12 +18,10 @@ const __dirname = path.dirname(__filename);
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-
   next();
 });
 app.use(
